e4864a0ea2dd9fb6efd96c24afc904e5ed65a9f9
bee-root <anthonyo.diculescu@gmail.com>
2024-05-20 08:18:55 +0300

    Modificare coduri Diculescu Anthonyo

--- app/test_biblioteca_pinguin.py
+++ app/test_biblioteca_pinguin.py
@@ -1,20 +1,46 @@
-def test_culoare_pinguin():
-    cul = b_animale.culoare_pinguin()
-    if cul == 'alb cu negru':
+def test_specie_magellan():
+    cul = b_animale.specie_magellan()
+    if cul == 'Magellan':
         assert True
     else:
         assert False
 
-def test_hrana_pinguin():
-    inf = b_animale.hrana_pinguin()
-    if inf == 'carnivor':
+def test_zona_magellan():
+    cul = b_animale.zona_magellan()
+    if cul == 'America de Sud':
         assert True
     else:
         assert False
 
-def test_invelisul_corpului_pinguin():
-    inf = b_animale.invelisul_corpului_pinguin()
-    if inf == 'blana':
+def test_detalii_magellan():
+    cul = b_animale.detalii_magellan()
+    if cul == '70 cm inaltime':
         assert True
     else:
         assert False
+
+
+
+
+
+def test_specie_gentoo():
+    cul = b_animale.specie_gentoo()
+    if cul == 'Gentoo':
+        assert True
+    else:
+        assert False
+
+def test_zona_gentoo():
+    cul = b_animale.zona_gentoo()
+    if cul == 'Peninsula Antartica':
+        assert True
+    else:
+        assert False
+
+def test_detalii_gentoo():
+    cul = b_animale.detalii_gentoo()
+    if cul == '90 cm inaltime':
+        assert True
+    else:
+        assert False
+
