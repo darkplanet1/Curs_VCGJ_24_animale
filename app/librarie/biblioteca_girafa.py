3c53e8f72d183458b91df326b9e50818e187ea37
Andree-Ioana <ioana.andreea@stud.etti.upb.ro>
2024-05-20 15:05:26 +0300

    Modificare coduri Andreea Vinereanu

---
+++ app/librarie/biblioteca_girafa.py
@@ -0,0 +1,21 @@
+def specie_girafa():
+    return("girafa")
+   
+    
+def zona_girafa():
+    return("asia")
+    
+def clasificare_girafa():
+    return("nubiana")
+    
+
+
+def specie_african():
+    return("African")
+   
+    
+def zona_african():
+    return("Africa")
+    
+def clasificare_african():
+    return("Nubiana")
