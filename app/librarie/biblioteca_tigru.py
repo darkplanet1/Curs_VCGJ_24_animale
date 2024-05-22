f84b7a6163229815d63ed2824913f09cdf0d16b9
LeoGeanovu <leogeanovu@gmail.com>
2024-05-23 00:54:31 +0300

    Modificare coduri Leo Geanovu

---
+++ app/librarie/biblioteca_tigru.py
@@ -0,0 +1,21 @@
+def specie_tigru():
+    return("siberian")
+   
+    
+def zona_tigru():
+    return("Rusia")
+    
+def clasificare_tigru():
+    return("carnivor")
+    
+
+
+def specie_siberian():
+    return("siberian")
+   
+    
+def zona_siberian():
+    return("Rusia")
+    
+def clasificare_siberian():
+    return("carnivor")
