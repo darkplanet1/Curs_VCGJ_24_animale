6da43e13422289875f5083ddffa5f3aaf2f101a0
HasanKassas <hasankassas14@gmail.com>
2024-05-23 23:41:57 +0300

    Updated biblioteca_animale.py with animal function

---
+++ app/librarie/biblioteca_animale.py
@@ -0,0 +1,21 @@
+def specie_Camila():
+    return("Arabian")
+   
+    
+def zona_Camila():
+    return("Liban")
+    
+def clasificare_Camila():
+    return("carnivor")
+    
+
+
+def specie_Arabian():
+    return("Arabian")
+   
+    
+def zona_Arabian():
+    return("Liban")
+    
+def clasificare_Arabian():
+    return("carnivor")
---
+++ app/test_biblioteca_Camila.py
@@ -0,0 +1,46 @@
+import librarie.biblioteca_Camila as bCamila
+
+def test_specie_Camila():
+    spec = bCamila.specie_Camila()
+    if spec == "Arabian" : 
+        assert True
+    else: 
+        assert False
+        
+
+def test_zona_Camila():
+    zona = bCamila.zona_Camila()
+    if zona == "Liban" :
+        assert True
+    else: 
+        assert False
+
+def test_clasificare_Camila():
+    clasificare = bCamila.clasificare_Camila()
+    if clasificare == "carnivor" :
+        assert True
+    else: 
+        assert False
+        
+        
+def test_specie_Arabian():
+    spec = bCamila.specie_Arabian()
+    if spec == "Arabian" : 
+        assert True
+    else: 
+        assert False
+        
+
+def test_zona_Arabian():
+    zona = bCamila.zona_Arabian()
+    if zona == "Liban" :
+        assert True
+    else: 
+        assert False
+
+def test_clasificare_Arabian():
+    clas = btigru.clasificare_Arabian()
+    if clas == "carnivor" :
+        assert True
+    else: 
+        assert False  
