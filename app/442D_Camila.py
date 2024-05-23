6899f4d3c4d72d40db9436ce2c0a0bc68b5f0c71
HasanKassas <hasankassas14@gmail.com>
2024-05-23 23:49:12 +0300

    Updated biblioteca_animale.py with animal function

--- app/442D_Camila.py
+++ app/442D_Camila.py
@@ -1,88 +1,88 @@
 from flask import Flask
-import librarie.biblioteca_camila
+import librarie.biblioteca_Camila
 
 app = Flask(__name__)
 
-print('442D_camila')
+print('442D_Camila')
 
 @app.route("/", methods=['GET'])
 def index():
-    ret = "<h1>Informatii despre camila 442D</h1>"
+    ret = "<h1>Informatii despre Camila 442D</h1>"
     return ret
 
-@app.route("/camila/", methods=['GET'])
-def get_camila():
-    ret = "<h1>camila<h1>"
+@app.route("/Camila/", methods=['GET'])
+def get_Camila():
+    ret = "<h1>Camila<h1>"
     ret += "Specie: "
-    ret += librarie.biblioteca_camila.specie_camila()
+    ret += librarie.biblioteca_Camila.specie_Camila()
     ret += "<br>"
     
     ret += "Zona: "
-    ret += librarie.biblioteca_camila.zona_camila()
+    ret += librarie.biblioteca_Camila.zona_Camila()
     ret += "<br>"
     
     ret += "Clasificare: "
-    ret += librarie.biblioteca_camila.clasificare_camila()
+    ret += librarie.biblioteca_Camila.clasificare_Camila()
     ret += "<br>"
     
     return ret
     
-@app.route("/camila/specie", methods=['GET'])
-def ia_specie_camila():
+@app.route("/Camila/specie", methods=['GET'])
+def ia_specie_Camila():
     ret = ""
-    ret += librarie.biblioteca_camila.specie_camila()
+    ret += librarie.biblioteca_Camila.specie_Camila()
     
     return ret
     
-@app.route("/camila/zona", methods=['GET'])
-def ia_zona_camila():
+@app.route("/Camila/zona", methods=['GET'])
+def ia_zona_Camila():
     ret = ""
-    ret += librarie.biblioteca_camila.zona_camila()
+    ret += librarie.biblioteca_Camila.zona_Camila()
     
     return ret
     
-@app.route("/camila/clasificare", methods=['GET'])
-def ia_clasificare_camila():
+@app.route("/Camila/clasificare", methods=['GET'])
+def ia_clasificare_Camila():
     ret = ""
-    ret += librarie.biblioteca_camila.clasificare_camila()
+    ret += librarie.biblioteca_Camila.clasificare_Camila()
     
     return ret
     
-@app.route("/siberian/", methods=['GET'])
-def get_siberian():
-    ret = "<h1>siberian<h1>"
+@app.route("/Arabian/", methods=['GET'])
+def get_Arabian():
+    ret = "<h1>Arabian<h1>"
     ret += "specie: "
-    ret += librarie.biblioteca_camila.specie_siberian()
+    ret += librarie.biblioteca_Camila.specie_Arabian()
     ret += "<br>"
     
     ret += "zona: "
-    ret += librarie.biblioteca_camila.zona_siberian()
+    ret += librarie.biblioteca_Camila.zona_Arabian()
     ret += "<br>"
     
     ret += "Clasificare: "
-    ret += librarie.biblioteca_camila.clasificare_siberian()
+    ret += librarie.biblioteca_Camila.clasificare_Arabian()
     ret += "<br>"
     
     return ret
     
-@app.route("/siberian/specie", methods=['GET'])
-def ia_specie_siberian():
+@app.route("/Arabian/specie", methods=['GET'])
+def ia_specie_Arabian():
     ret = ""
-    ret += librarie.biblioteca_camila.specie_siberian()
+    ret += librarie.biblioteca_Camila.specie_Arabian()
     
     return ret
     
-@app.route("/siberian/zona", methods=['GET'])
-def ia_zona_siberian():
+@app.route("/Arabian/zona", methods=['GET'])
+def ia_zona_Arabian():
     ret = ""
-    ret += librarie.biblioteca_camila.zona_siberian()
+    ret += librarie.biblioteca_Camila.zona_Arabian()
     
     return ret
     
-@app.route("/siberian/clasificare", methods=['GET'])
-def ia_clasificare_siberian():
+@app.route("/Arabian/clasificare", methods=['GET'])
+def ia_clasificare_Arabian():
     ret = ""
-    ret += librarie.biblioteca_camila.clasificare_siberian()
+    ret += librarie.biblioteca_Camila.clasificare_Arabian()
     
     return ret
 
